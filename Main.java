95af22c0497a3009c3efaacf29d6624063e66499
Brian Wong <brianwong@zipcoders-MacBook-Pro.local>
2019-02-06 13:32:28 -0500

    Finished SumOfInput

--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
-
-    public static void main(String[] args){
-
+  
+    
+    public static void main(){
+    Scanner scanner = new Scanner(System.in);
+    int i;
+    int input = 0;
+    int total = 0;
+    System.out.println("Enter a number from 1 to n in order to get the sum of the numbers");
+    input = scanner.nextInt();
+        for(i = 1; i <= input; i++) {
+        total = total + i;
+        if(i == input) {
+        System.out.println("Your sum is " + total); }
+        
+    }
+   
     }
 }
